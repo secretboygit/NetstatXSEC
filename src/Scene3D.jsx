aad81a5c1da6956101b2c4bd9bda9354476b18db
Mr.Mystix <mr.mystix06@gmail.com>
2026-05-13 20:09:43 +0530

    Optimize 3D model size

--- src/Scene3D.jsx
+++ src/Scene3D.jsx
@@ -1,31 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import {
-  Float,
-  OrbitControls,
-  Stars,
-  useGLTF,
-} from "@react-three/drei";
-
-function GirlModel() {
-  const { scene } = useGLTF(
-    `${import.meta.env.BASE_URL}girl.glb`
-  );
-
-  return (
-    <Float
-      speed={1.5}
-      rotationIntensity={0.2}
-      floatIntensity={1}
-    >
-      <primitive
-  object={scene}
-  scale={5}
-  position={[0, -4, -2]}
-  rotation={[0, 0, 0]}
-/>
-    </Float>
-  );
-}
+import { OrbitControls, Stars } from "@react-three/drei";
 
 export default function Scene3D() {
   return (
@@ -38,19 +12,12 @@ export default function Scene3D() {
         width: "100%",
         height: "100%",
         zIndex: 0,
-        pointerEvents: "none",
       }}
     >
-      <ambientLight intensity={1.8} />
+      <ambientLight intensity={1.5} />
 
       <directionalLight
         position={[5, 5, 5]}
-        intensity={3}
-        color="#ffffff"
-      />
-
-      <pointLight
-        position={[-5, 3, 5]}
         intensity={2}
         color="#00ff73"
       />
@@ -58,24 +25,19 @@ export default function Scene3D() {
       <Stars
         radius={100}
         depth={60}
-        count={2500}
+        count={3000}
         factor={4}
+        saturation={0}
         fade
         speed={1}
       />
-
-      <GirlModel />
 
       <OrbitControls
         enableZoom={false}
         enablePan={false}
         autoRotate
-        autoRotateSpeed={0.5}
+        autoRotateSpeed={0.4}
       />
     </Canvas>
   );
 }
-
-useGLTF.preload(
-  `${import.meta.env.BASE_URL}girl.glb`
-);
